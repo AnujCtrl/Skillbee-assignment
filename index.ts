a1c1b19abc939208ae56429cd43c320a86ee7d8a
AnujCtrl <anujpatil200@gmail.com>
2023-04-05 15:58:55 +0530

    Added types FIxed the UI Initalized workflow

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 
-import eta from "eta";
-import fs from "fs";
-import path from "path";
+import eta = require("eta");
+import path = require("path");
+import fs = require("fs");
+import axios from "axios";
 
 interface Activity {
   activity: string;
@@ -13,32 +14,37 @@ interface Activity {
   accessibility: number;
 }
 
-fetch("https://www.boredapi.com/api/activity")
-  .then((response) => response.json())
-  .then((data: Activity) => {
-    // Log the retrieved data to the console
-    console.log(data);
-  })
-  .catch((error) => {
-    // Handle any errors that occur
+async function getActivity(): Promise<Activity> {
+  try {
+    const response = await axios.get<Activity>(
+      "https://www.boredapi.com/api/activity"
+    );
+    return response.data;
+  } catch (error) {
     console.error(error);
-  });
+    throw error;
+  }
+}
 
-const data = {
-  activity: "Explore the nightlife of your city",
-  type: "social",
-  participants: 1,
-  price: 0.1,
-  link: "",
-  key: "2237769",
-  accessibility: 0.32,
-};
-export async function generateHTML(data, outputPath) {
+// const data = {
+//   activity: "Explore the nightlife of your city",
+//   type: "social",
+//   participants: 1,
+//   price: 0.1,
+//   link: "",
+//   key: "2237769",
+//   accessibility: 0.32,
+// };
+export async function generateHTML() {
   const templatePath = path.join(__dirname, "index.html");
   const template = await fs.promises.readFile(templatePath, "utf-8");
+  const data = await getActivity();
   console.log(data);
 
-  const rendered = eta.render(template, { data });
-  await fs.promises.writeFile(outputPath, rendered);
+  const rendered = eta.render(template, data);
+  await fs.promises.writeFile(
+    __dirname + data.activity.replace(/\s+/g, "")+".html",
+    rendered
+  );
 }
 
